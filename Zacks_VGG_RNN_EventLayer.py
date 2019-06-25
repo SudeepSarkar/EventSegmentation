aabf144152d4d6a7ab1a562bd2b6fae8b64c6869
Sarkar <sarkar@techs-MacBook-Pro-2.local>
2019-06-25 09:21:40 -0400

    update

--- Zacks_VGG_RNN_EventLayer.py
+++ Zacks_VGG_RNN_EventLayer.py
@@ -282,8 +282,8 @@ with tf.compat.v1.Session() as sess:
                 segCount += 1
                 ret = sess.run([train_op, sseLoss, sseLoss1, curr_state1, fc1], feed_dict = {inputs: x_train, is_training: True, init_state1: new_state, learning_rate:lr})
                 new_state = ret[3]
-				print (ret[1])
-				print (ret[3])
+                print (ret[1])
+                print (ret[3])
                 if activeLearning:
                     if ret[1]/avgPredError > 1.5:
                         lr = 1e-8
