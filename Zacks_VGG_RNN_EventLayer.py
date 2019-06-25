5242b67eeaed4678d503d67072d133bf7702a60e
Sarkar <sarkar@VPN-168-221.vpn.usf.edu>
2019-06-25 00:10:19 -0400

    update

--- Zacks_VGG_RNN_EventLayer.py
+++ Zacks_VGG_RNN_EventLayer.py
@@ -275,12 +275,12 @@ with tf.compat.v1.Session() as sess:
             # LSTM
             new_state = np.random.uniform(-0.5,high=0.5,size=(1,2*n_hidden1))
             avgPredError = 0
-			vidName, minibatches = loadMiniBatch(miniBatchPath)
-			segCount = 0
-			predError = collections.deque(maxlen=30)
-			print('Video:', vidName)
-			print( vgg16_Features[1,:])
-			for x_train in minibatches:
+            vidName, minibatches = loadMiniBatch(miniBatchPath)
+            segCount = 0
+            predError = collections.deque(maxlen=30)
+            print('Video:', vidName)
+            print( vgg16_Features[1,:])
+            for x_train in minibatches:
                 segCount += 1
                 ret = sess.run([train_op, sseLoss, sseLoss1, curr_state1, fc1], feed_dict = {inputs: x_train, is_training: True, init_state1: new_state, learning_rate:lr})
                 new_state = ret[3]
