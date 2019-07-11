fbe2551bcbf0b8f3d577e864d55a556003fdfa2c
Sarkar <sarkar@techs-MacBook-Pro-2.local>
2019-07-11 00:31:52 -0400

    update

--- Zacks_VGG_RNN_EventLayer.py
+++ Zacks_VGG_RNN_EventLayer.py
@@ -198,10 +198,9 @@ curr_state1 = init_state1
 
 # ----------------------------------------------------- #
 #             SETTING UP VGG
-r, g, b = tf.split(axis=3, num_or_size_splits=3, value=inputs * 255.0)
-VGG_MEAN = [103.939, 116.779, 123.68]
+r, g, b = tf.split(axis=3, num_or_size_splits=3, value=inputs)
+VGG_MEAN = [103.939, 116.779, 123.68]/255
 VGG_inputs = tf.concat(values=[b - VGG_MEAN[0], g - VGG_MEAN[1], r - VGG_MEAN[2]], axis=3)
-VGG_inputs = VGG_inputs/255.0
 #tf.summary.image(name='Input Image', tensor= VGG_inputs)
 
 vgg16_Features, end_points = vgg_16(inputs=VGG_inputs, is_training=True,
