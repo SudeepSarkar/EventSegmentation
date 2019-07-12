4f62431b449914a8fde31f863eee92185c2579e6
Sarkar <sarkar@techs-MacBook-Pro-2.local>
2019-07-11 21:39:38 -0400

    update

--- Zacks_VGG_RNN_EventLayer.py
+++ Zacks_VGG_RNN_EventLayer.py
@@ -210,7 +210,8 @@ h_1, curr_state1 = lstm_cell(W_lstm1, b_lstm1, 1.0, RNN_inputs, curr_state1)
 fc1 = h_1
 tf.summary.image(name='LSTM output', tensor= tf.reshape(fc1, (-1, 64, 64, 1)))
 
-sseLoss1 = tf.square(tf.subtract(fc1[0,:], vgg16_Features[1,:]))
+#sseLoss1 = tf.square(tf.subtract(fc1[0,:], vgg16_Features[1,:]))
+sseLoss1 = tf.abs(tf.subtract(fc1[0,:], vgg16_Features[1,:]))
 mask = tf.greater(sseLoss1, learnError * tf.ones_like(sseLoss1))
 sseLoss1 = tf.multiply(sseLoss1, tf.cast(mask, tf.float32))
 sseLoss = tf.reduce_mean(sseLoss1)
